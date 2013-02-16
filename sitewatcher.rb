86043b3619ef9d2fe70239f3b42fee87a2f8622e
Graeme Douglas <gdouglas.dev@gmail.com>
2013-02-15 23:20:34 -0800

    Add more basic scaffolding

--- sitewatcher.rb
+++ sitewatcher.rb
@@ -1,37 +1,16 @@
+#!/usr/bin/env ruby
+###	sitewatcher.rb
+###	Command line tool for monitoring content changes of a web page locally.
+###	
+###	Written by Graeme Douglas.
+###	
+###	Please refer to license.md for licensing information.
+
 require 'pstore'
-require 'net/http'
-require 'net/https'
-require 'uri'
 require 'open-uri'
+require 'optparse'
 
-def get_page_request(domain, location, type='http')
-	url = URI.parse(domain+location)
-	
-	req = nil
-	requester = nil
-	if type == 'https'
-		req = Net::HTTPS::Get.new(url.path)
-		requester = Net::HTTPS.new(url.host, url.port)
-	else
-		req = Net::HTTP::Get.new(url.path)
-		requester = Net::HTTP.new(url.host, url.port)
-	end
-p url.path
-p req
-p requester
-	#req.add_field('Cache-Control', 'no-cache')
-	
-	return req, requester
-end
-
-def handle_response(response)
-	if response.code == 200
-		return response.body
-	else
-		die("Response code: #{response.code}")
-	end
-end
-
+### Functions ##################################################################
 def get_page_contents(url)
 	string = ""
 	open(url, "Cache-Control"=>"no-cache") do |file|
@@ -42,7 +21,7 @@ end
 
 def site_changed?(domain, location = '/')
 	# TODO: Make this work with HTTPS.
-	source = Net::HTTP.get(domain, location)
+	source = get_page_contents(domain+location)
 	
 	hashed = source.hash
 	
@@ -56,18 +35,12 @@ def site_changed?(domain, location = '/')
 end
 
 def instant_watch(domain, location = '/')
-	#request, requester = get_page_request(domain, location)
-	
-	#response = requester.request(request)
-	#init_source = handle_response(response)
 	init_source = get_page_contents(domain+location)
 	source = String.new(init_source)
 	
 	while init_source == source
 		sleep 2		# TODO: Make this configurable
 		
-		#response = requester.request(request)
-		#source = handle_response
 		source = get_page_contents(domain+location)
 		
 		if init_source == source
@@ -76,8 +49,22 @@ def instant_watch(domain, location = '/')
 	end
 	puts "Changed!"
 end
+################################################################################
 
-domain = 'stackoverflow.com'
-domain = 'http://www.reddit.com'
+### Option Handling ############################################################
+options = {}
+OptionParser.new do |opts|
+	opts.banner = "Usage: sitewatcher.rb"
+end.parse!
+################################################################################
 
-instant_watch(domain)
+### If this file executed ######################################################
+if __FILE__ == $0
+	domain = 'http://www.reddit.com'
+	
+	if ARGV[0] != nil
+		domain = ARGV[0]
+	end
+	instant_watch(domain)
+end
+################################################################################
