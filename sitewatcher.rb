98e15870cffb5581b76df28bcb22be94e418fc8f
Graeme Douglas <gdouglas.dev@gmail.com>
2013-04-14 22:49:58 -0700

    Add option parsing, refine persistant watch
    It is pretty much in a totally usable state at the moment.
    instant_watch is a little slow to start, but not too bad.

--- sitewatcher.rb
+++ sitewatcher.rb
@@ -9,9 +9,21 @@
 require 'pstore'
 require 'open-uri'
 require 'optparse'
+require 'ostruct'
+
+### Constants ##################################################################
+## Execution types.
+EXEC_TYPE_LOOKUP  = 0
+EXEC_TYPE_INSTANT = 1
+################################################################################
+
+### Global Variables ###########################################################
+$store = PStore.new("sitewatcher.pstore")
+$options = OpenStruct.new
+################################################################################
 
 ### Functions ##################################################################
-def get_page_contents(url)
+def get_source_contents(url)
 	string = ""
 	open(url, "Cache-Control"=>"no-cache") do |file|
 		file.each_line {|line| string << line}
@@ -19,52 +31,102 @@ def get_page_contents(url)
 	return string
 end
 
-def site_changed?(domain, location = '/')
-	# TODO: Make this work with HTTPS.
-	source = get_page_contents(domain+location)
+def persistant_source_changed?(domain, location = '/')
+	source = get_source_contents(domain+location)
 	
-	hashed = source.hash
+	hashed = source		# TODO: Hash using predictable hash function.
 	
-	store = PStore.new("sitewatcher.pstore")
-	
-	if hashed == store[domain + location]
-		return false;
-	else
-		return true;
+	$store.transaction do
+		current = $store[domain + location]
+		if current == nil
+			$store[domain + location] = hashed
+			return 'New value stored.'
+		elsif hashed == current
+			return false;
+		else
+			$store[domain + location] = hashed
+			return true;
+		end
 	end
 end
 
 def instant_watch(domain, location = '/')
-	init_source = get_page_contents(domain+location)
+	init_source = get_source_contents(domain+location)
 	source = String.new(init_source)
 	
-	while init_source == source
-		sleep 2		# TODO: Make this configurable
+	count = 0
+	
+	while init_source == source and count < $options.request_limit
+		sleep $options.wait_time
 		
-		source = get_page_contents(domain+location)
+		source = get_source_contents(domain+location)
 		
 		if init_source == source
 			puts "No change at #{domain+location}"
 		end
+		count+=1
 	end
-	puts "Changed!"
+	
+	if init_source != source
+		puts "Changed!"
+	else
+		puts "No change found before request limit reached."
+	end
 end
 ################################################################################
 
 ### Option Handling ############################################################
-options = {}
+## Default options.
+$options.exec_type = EXEC_TYPE_INSTANT
+$options.wait_time = 2
+$options.request_limit = 10000000
+$options.source = "http://www.reddit.com"	# TODO: Must be better way?
+
 OptionParser.new do |opts|
-	opts.banner = "Usage: sitewatcher.rb"
+	opts.banner = "Usage: sitewatcher.rb -s <site> [options]"
+	
+	## Options parsing.
+	opts.on("-h", "--help",
+	 "Show help message.") do |v|
+		puts opts
+		exit()
+	end
+	
+	opts.on("-s", "--source <s>", String,
+	 "Set source location") do |s|
+		$options.source = s
+	end
+	
+	opts.on("-t", "--wait-time <t>", Integer,
+	 "Number of seconds to wait between requests") do |t|
+		$options.wait_time = t
+	end
+	
+	opts.on("-p", "--persistence-check",
+	 "Check if source changed from cache") do
+		$options.exec_type = EXEC_TYPE_LOOKUP
+	end
+	
+	opts.on("-l", "--limit-requests <l>", Integer,
+	 "Limit number of checks performed") do |l|
+		$options.request_limit = l
+	end
 end.parse!
 ################################################################################
 
 ### If this file executed ######################################################
 if __FILE__ == $0
-	domain = 'http://www.reddit.com'
-	
-	if ARGV[0] != nil
-		domain = ARGV[0]
+	if EXEC_TYPE_INSTANT == $options.exec_type
+		instant_watch($options.source)
+	else
+		result = persistant_source_changed?($options.source)
+		if true == result
+			puts "Changed!"
+		elsif false == result
+			puts "Not changed."
+		else
+			puts result
+		end
 	end
-	instant_watch(domain)
 end
 ################################################################################
